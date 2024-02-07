4dceae9b3f0fa680b60113695ad2beb3ef2e6933
Sidhant Singh <ns865723@gmail.com>
2024-02-08 01:11:34 +0530

    admin dashboard completed

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>singhsidhant</span>
+      <span>© Sidhant Singh Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/notfound/notfound.tsx
@@ -0,0 +1,13 @@
+import "./notfound.scss";
+
+const NotFound = () => {
+  return (
+    <div className="notfound">
+      <div className="notfound-content">
+        Page not found
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
